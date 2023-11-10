c4e9607a2bdd9c5a56c09fc3ad45bb79843770cf
velllu <91963404+velllu@users.noreply.github.com>
2023-11-10 20:21:10 +0100

    Added basic movement

--- src/game.py
+++ src/game.py
@@ -1,7 +1,15 @@
 import pygame
+import time
 import sys
 from enum import Enum
 from typing import Tuple
+
+# Examples
+# Input: (5, 2) and (2, 2)
+# Output: (7, 4)
+def add_positions(tuple1: Tuple[int, int], tuple2: Tuple[int, int]) -> Tuple[int, int]:
+    print(tuple1, tuple2)
+    return (tuple1[0] + tuple2[0], tuple1[1] + tuple2[1])
 
 class Cell(Enum):
     EMPTY = (0, 0, 255)
@@ -9,10 +17,10 @@ class Cell(Enum):
     APPLE = (255, 0, 0)
 
 class Direction(Enum):
-    UP = 0
-    LEFT = 1
-    RIGHT = 2
-    DOWN = 3
+    UP = (0, -1)
+    LEFT = (-1, 0)
+    RIGHT = (1, 0)
+    DOWN = (0, 1)
 
 class Snake:
     def __init__(self, size: Tuple[int, int], board_size: int, cell_size: int) -> None:
@@ -22,6 +30,7 @@ class Snake:
         self.current_direction: Direction = Direction.RIGHT
         self.board_size = board_size
         self.cell_size = cell_size
+        self.events = []
 
         self.board: list[list[Cell]] = [[Cell.EMPTY for _ in range(board_size)] for _ in range(board_size)]
 
@@ -53,18 +62,40 @@ class Snake:
                     self.cell_size)
                 )
 
+    def is_position_valid(self, position: Tuple[int, int]) -> bool:
+        if self.board_size > position[0] and position[0] >= 0 \
+            and self.board_size > position[1] and position[1] >= 0:
+
+            return True
+
+        return False
+
     def tick(self) -> None:
-        # TODO: Add left, down, and up, and check if the next position is gonna be valid
-        # or not
-        if self.current_direction == Direction.RIGHT and self.board_size > self.snake_position[0] + 1:
-            self.set_tile((self.snake_position[0] + 1, self.snake_position[1]), Cell.SNAKE)
+        if self.is_position_valid((self.snake_position[0] + 1, self.snake_position[1])):
+            print(self.current_direction.name)
+            self.set_tile(add_positions(self.snake_position, self.current_direction.value), Cell.SNAKE)
+
+        for event in self.events:
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_w:
+                    self.current_direction = Direction.UP
+                elif event.key == pygame.K_a:
+                    self.current_direction = Direction.LEFT
+                elif event.key == pygame.K_s:
+                    self.current_direction = Direction.DOWN
+                elif event.key == pygame.K_d:
+                    self.current_direction = Direction.RIGHT
 
         self.draw_board()
                 
         pygame.display.flip()
+        pygame.time.delay(600)
+
+    def update_events(self):
+        self.events = pygame.event.get()
 
     def should_close(self) -> bool:
-        for event in pygame.event.get():
+        for event in self.events:
             if event.type == pygame.QUIT:
                 return True
 
--- src/main.py
+++ src/main.py
@@ -5,4 +5,5 @@ if __name__ == "__main__":
     snake.spawn_snake()
 
     while not snake.should_close():
+        snake.update_events()
         snake.tick()
